3e21f70864ec4a38f6523a2bb3297dc0e5ec4e25
Reiner Balschun <152055446+ReinerBalschun@users.noreply.github.com>
2024-01-11 20:22:55 +0100

    Upload LF 5 11.01.24
    Time: 08:22 PM

---
+++ LF 5/Struktogramme/Python Lösungen/Aufgabe 3 Kapitel 4.py
@@ -0,0 +1,14 @@
+# Initialisierung der Variablen
+x = 1
+y = 3
+
+# Schleife, die solange läuft, bis die Summe von x und y größer oder gleich 50 ist
+while x + y < 50:
+    # Die Summe von x und y berechnen
+    summe = x + y
+    
+    # Den Wert von x um 2 erhöhen
+    x += 2
+
+# Nach der Schleife den aktuellen Wert von x ausgeben
+print("Der finale Wert von x ist:", x)
---
+++ LF 5/Struktogramme/Python Lösungen/Radius.py
@@ -0,0 +1,4 @@
+radius = input("Geben Sie den Radius ein: ")
+radius = float(radius)
+flaeche = 3.1415 * radius * radius
+print(flaeche)
